e234893e912a0c5f241cc744dd4c334f9e1833cc
scola <joseph.scola@uvsq.fr>
2023-07-17 11:26:59 +0200

    Initializing gh-pages branch

---
+++ GraphParams.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 17:45:50 2023
+
+@author: scola
+"""
+
+KaufmannFederspiel = {
+    'background' : "black",
+    'CAMAIEU' : ["#779CFF", "#77C3FF", "#77E8FF", "#77FFD8", "#77FF9A", "#ABFF77", "#D6FF77", "#FFFD77", "#FFD877", "#FFB377", "#FF8777"],
+    'linecolor' : "dark gray", #"#A2B5C7"           
+    'R2TFS_ratio' : 20,
+    'TFS_fill' : "#EAF3FB",
+    'TFS_outline' : "#A2B5C7",
+    'R2RS_ratio' : 15,
+    # Vertex1
+    'enlarge' : 5/4,
+    'Vertex1color' : 'red'
+    }
+
+rythmologie = {
+    'background' : "black",
+    'CAMAIEU' : ["#779CFF", "#77C3FF", "#77E8FF", "#77FFD8", "#77FF9A", "#ABFF77", "#D6FF77", "#FFFD77", "#FFD877", "#FFB377", "#FF8777"],
+    'linecolor' : "#A2B5C7",
+    'R2TFS_ratio' : 20,
+    'TFS_fill' : "#EAF3FB",
+    'TFS_outline' : "#A2B5C7",
+    'R2RS_ratio' : 15,
+    # Vertex1
+    'enlarge' : 7/4,
+    'Vertex1color' : 'red',
+    'MainCycleVisible' : True,
+    'SecondaryCyclesVisible' : False
+    }
